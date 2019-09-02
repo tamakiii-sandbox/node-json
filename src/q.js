0258d5ed3a9805fb9abd0da484bf178c2b737528
tamakiii <tamakiii@users.noreply.github.com>
2019-09-02 20:41:40 +0900

    impl args and query

--- src/q.js
+++ src/q.js
@@ -1,3 +1,19 @@
-const json = JSON.parse(require('fs').readFileSync('/dev/stdin', 'utf8'));
+const fs = require('fs');
+const jsonQuery = require('json-query');
+const commandLineArgs = require('command-line-args');
 
-console.log(json);
+const options = commandLineArgs([
+  { name: 'query', type: String, multiple: true, defaultOption: true },
+  { name: 'beautify', alias: 'b', type: Boolean, defaultValue: false },
+]);
+
+const stdin = fs.readFileSync('/dev/stdin', 'utf8');
+const data = JSON.parse(stdin);
+
+const result = jsonQuery(options.query, { data });
+
+if (options.beautify) {
+  console.log(JSON.stringify(result, null, 2));
+} else {
+  console.log(JSON.stringify(result));
+}
