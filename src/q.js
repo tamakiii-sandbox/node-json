7f54cc586743be0d652d0f7822564ec0f917978a
tamakiii <tamakiii@users.noreply.github.com>
2019-09-02 21:19:11 +0900

    using --after and --before

--- src/q.js
+++ src/q.js
@@ -4,16 +4,27 @@ const commandLineArgs = require('command-line-args');
 
 const options = commandLineArgs([
   { name: 'query', type: String, multiple: true, defaultOption: true },
-  { name: 'beautify', alias: 'b', type: Boolean, defaultValue: false },
+  { name: 'beautify', alias: 'y', type: Boolean, defaultValue: false },
+  { name: 'before', alias: 'b', type: String },
+  { name: 'after', alias: 'a', type: String },
 ]);
 
 const stdin = fs.readFileSync('/dev/stdin', 'utf8');
-const data = JSON.parse(stdin);
 
-const result = jsonQuery(options.query, { data });
+var data = JSON.parse(stdin);
+
+if (options.before) {
+  data = (new Function('data', options.before))(data);
+}
+
+data = jsonQuery(options.query, { data });
+
+if (options.after) {
+  data = (new Function('data', options.after))(data);
+}
 
 if (options.beautify) {
-  console.log(JSON.stringify(result, null, 2));
+  console.log(JSON.stringify(data, null, 2));
 } else {
-  console.log(JSON.stringify(result));
+  console.log(JSON.stringify(data));
 }
